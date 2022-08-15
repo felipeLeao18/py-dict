141fb241f60cc332093d5b7a8aff68b96199bb39
felipeLeao18 <flpleao1807@gmail.com>
2022-08-15 19:27:34 -0300

    test: add create filter test

---
+++ test_create_filter.py
@@ -0,0 +1,12 @@
+from app import create_filter
+
+def test_create_filter_with_no_arguments():
+    assert create_filter() == []
+
+def test_create_filter_with_several_arguments():
+    assert create_filter('_id;title;description') == ['_id', 'title', 'description']
+
+def test_create_filter_with_arguments_and_empy_spaces():
+    assert create_filter('  _id ; title ; description ; release_year') == ['_id', 'title', 'description', 'release_year']
+
+   
