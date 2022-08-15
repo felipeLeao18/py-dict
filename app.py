af034ab2e043551dd998dfb77e0e09f3c07cb967
felipeLeao18 <flpleao1807@gmail.com>
2022-08-15 19:26:52 -0300

    feat: add parse games list method

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ def get_games_full_list() -> List[Dict[str, str]]:
     
 def create_pagination(page = 1, per_page = 10) -> Dict[str, int]:
 
-    start_range = (page - 1)  * per_page
-    final_range = (start_range + per_page) -1
+    start_range = (int(page) - 1)  * int(per_page)
+    final_range = int((start_range + per_page)) - 1
 
     return {'start_range': start_range, 'final_range': final_range}
+
+def parse_games_full_list(page = 1, per_page = 10) -> List[Dict[str, str]]:
+    games_data = get_games_full_list()
+    pagination = create_pagination(page, per_page)
+
+    start_range, final_range = pagination['start_range'], pagination['final_range']
+    games_fetched = games_data[int(start_range) : int(final_range)]
+
+    return games_fetched
+    
