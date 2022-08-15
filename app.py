ebf3a29f6f4cd7605d539f69ac29821b9f65da2f
felipeLeao18 <flpleao1807@gmail.com>
2022-08-15 19:24:37 -0300

    feat: add create pagination method

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ def get_games_full_list() -> List[Dict[str, str]]:
     games = res.json()
     return games
     
+def create_pagination(page = 1, per_page = 10) -> Dict[str, int]:
+
+    start_range = (page - 1)  * per_page
+    final_range = (start_range + per_page) -1
+
+    return {'start_range': start_range, 'final_range': final_range}
